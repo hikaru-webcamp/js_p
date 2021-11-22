5fee57babf62b5b8c4ec4a8784afe70719e0ce46
hikaru <skyvader0524@gmail.com>
2021-11-23 00:06:09 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// var vall = "var変数";
+// console.log(vall);
+
+// vall  = "test";
+// console.log(vall);
+
+// let val2 = "let変数";
+// console.log(val2)
+
+// const val4 = {
+//   name: "じゃけ",
+//   age: 28,
+// };
+
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+console.log(val5);
+val5.push("mounkey");
+console.log(val5);
